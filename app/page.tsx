1b83116f02de38775d7d60bcacd9b1e1f3671644
Jenesh <jeneshrambo18@gmail.com>
2025-02-03 23:18:37 +0530

    Initial commit - Portfolio for Jenesh Devganiya

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Home from '@/components/Home/Home'
+import React from 'react'
+
+const homePage = () => {
+  return (
+    <div className='m-3'><Home /></div>
+  )
+}
+
+export default homePage
